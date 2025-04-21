5517b336a6e03c320cba746492abf358ad3c7a63
FadwaElouah <fadwaelouah@gmail.com>
2025-04-21 10:24:30 +0100

    terminer

--- src/App.vue
+++ src/App.vue
@@ -11,3 +11,17 @@ export default {
   }
 }
 </script>
+
+<style>
+html, body, #app {
+  height: 100%;
+  margin: 0;
+}
+body {
+  font-family: 'Segoe UI', sans-serif;
+  color: #fff;
+  background: linear-gradient(135deg,rgba(30, 30, 47, 0.8),rgb(245, 143, 96));
+  background-attachment: fixed;
+}
+</style>
+
